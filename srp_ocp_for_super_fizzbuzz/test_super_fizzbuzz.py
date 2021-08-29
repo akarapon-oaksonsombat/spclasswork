bd2e71b07e0ff5febe1bd72b6b21b5e269489a7e
Akarapon Oaksonsombat <47691236+akarapon-oaksonsombat@users.noreply.github.com>
2021-08-29 13:25:53 +0700

    Update TestSuperFizzBuzz

--- srp_ocp_for_super_fizzbuzz/test_super_fizzbuzz.py
+++ srp_ocp_for_super_fizzbuzz/test_super_fizzbuzz.py
@@ -4,19 +4,19 @@ from super_fizzbuzz import super_fizzbuzz
 
 class TestSuperFizzBuzz(unittest.TestCase):
     def test_integer(self):
-        for number in range(1, 10000, 1):
-            result = ""
-            if number % 3 == 0:
-                result += "Fizz"
-            if number % 9 == 0:
-                result += "Fizz"
-            if number % 5 == 0:
-                result += "Buzz"
-            if number % 25 == 0:
-                result += "Buzz"
-            if result == "":
-                result = "NoFizzBuzz"
-            self.assertEqual(super_fizzbuzz(number), result)
+        for test_number in range(1, 10000, 1):
+            expected_result = ""
+            if test_number % 3 == 0:
+                expected_result += "Fizz"
+            if test_number % 9 == 0:
+                expected_result += "Fizz"
+            if test_number % 5 == 0:
+                expected_result += "Buzz"
+            if test_number % 25 == 0:
+                expected_result += "Buzz"
+            if expected_result == "":
+                expected_result = "NoFizzBuzz"
+            self.assertEqual(super_fizzbuzz(test_number), expected_result)
 
 
 if __name__ == '__main__':
