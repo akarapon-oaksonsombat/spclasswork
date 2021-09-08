174d60f333dfc560b849d99779c7583c316ab787
Akarapon Oaksonsombat <akarapon.oaksonsombat@gmail.com>
2021-09-08 23:09:48 +0700

    Update "file opt mock and stub"

--- file_opt_mock_and_stub/file_opt.py
+++ file_opt_mock_and_stub/file_opt.py
@@ -3,15 +3,7 @@ import os.path
 
 
 def rm_when_lower(filename):
-    # print(f"received filename => {filename}")
     if filename.isupper():
-        # print(f"\"{filename}\" => is upper string")
         return
-    # print(f"isFile => {os.path.isfile(filename)}")
     if os.path.isfile(filename):
-        # print(f"remove filename => {filename}")
         os.remove(filename)
-
-# if __name__ == '__main__':
-#     fn = input('INPUT : ')
-#     print(rm_when_lower(fn))
